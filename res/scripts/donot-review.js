8166e4fe647ce0ded03f9acf9d19d795a5c005e5
techryon <techryon@gmail.com>
2019-05-16 22:13:52 +1200

    sample code for creating html elements

--- res/scripts/donot-review.js
+++ res/scripts/donot-review.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	onload_image();
-
+  createDiv();
   //The array has been created so its available in the memory during the game
 //  for (var faceCard=0; faceCard<=51; faceCard++){
  //   faceCardsArray[faceCard] = new Image(); //Every element of the array is defined as a new image
@@ -20,3 +20,16 @@ function onload_image() {
     }
   }
   
+function createDiv() {
+  var parentDiv = document.getElementById("gallery");
+  for(i=1; i<=2; i++){
+  var div = document.createElement("div");
+  div.setAttribute("class", "gallery__item");
+  div.setAttribute("id", ("gallery_item"+i));
+  parentDiv.appendChild(div);
+  var img = document.createElement("img");
+  div.appendChild(img);
+  img.setAttribute("class", "gallery__img");
+  img.setAttribute("id", ("gallery_img"+i));
+  }
+}
