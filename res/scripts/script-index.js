2c74c0229fb67d345a99e47dd1a083a831f54304
techryon <techryon@gmail.com>
2019-05-16 22:48:52 +1200

    divs created using javascript

--- res/scripts/script-index.js
+++ res/scripts/script-index.js
@@ -1,18 +1,28 @@
 //AddEventListener to DOMContentLoaded, so the image waits to load
 //after the page is loaded
 document.addEventListener("DOMContentLoaded", function(event) {
-	onload_image();
+ // onload_image();
+  createDiv();
  
 });
 
-//Function to load default images. Looping it so it loads the same image in 36cells
-//This function gets the image using Id and adds the source attributes 
-function onload_image() {
-    for(var defaultImage=1; defaultImage<=36; defaultImage++){
-      var src = "./res/images/card-background.png";
-      var img = document.getElementById("cell__img" + defaultImage);
-      img.src = src;
-      
+  //Function to create the div elements for the grid
+  function createDiv() {
+    var parentDiv = document.getElementById("container");
+    for(i=1; i<=36; i++){
+    var div = document.createElement("div");
+    div.setAttribute("class", "container__cell");
+    div.setAttribute("id", ("container_cell"+i));
+    parentDiv.appendChild(div);
+    createImg(i, div);
     }
+  }
+  //Function to create the img element for the grid
+  function createImg(i, div) {
+    var img = document.createElement("img");
+    div.appendChild(img);
+    img.setAttribute("class", "cell__img");
+    img.setAttribute("id", ("cell__img"+i));
+    img.setAttribute("src", "./res/images/card-background.png")
   }
   
