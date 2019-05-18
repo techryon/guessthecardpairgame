963f19a3cd466e4e1280d9e3bf542d858ca5ef0c
techryon <techryon@gmail.com>
2019-05-18 22:31:19 +1200

    div added, loop variabled

--- res/scripts/script-index.js
+++ res/scripts/script-index.js
@@ -9,20 +9,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //Function to create the div elements for the grid
   function createDiv() {
     var parentDiv = document.getElementById("container");
-    for(i=1; i<=36; i++){
+    for(let index=1; index<=36; index++){
     var div = document.createElement("div");
     div.setAttribute("class", "container__cell");
-    div.setAttribute("id", ("container_cell"+i));
+    div.setAttribute("id", ("container_cell"+index));
     parentDiv.appendChild(div);
-    createImg(i, div);
+    createImg(index, div);
     }
   }
   //Function to create the img element for the grid
-  function createImg(i, div) {
+  function createImg(index, div) {
     var img = document.createElement("img");
     div.appendChild(img);
     img.setAttribute("class", "cell__img");
-    img.setAttribute("id", ("cell__img"+i));
+    img.setAttribute("id", ("cell__img"+index));
     img.setAttribute("src", "./res/images/card-background.png")
   }
   
