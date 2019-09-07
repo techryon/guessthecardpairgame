921a73032f743b56206b0417137ab8d2bb0b52e1
techryon <techryon@gmail.com>
2019-09-07 17:57:22 +1200

    Animations and stuff done

---
+++ res/scripts/script-index.js
@@ -0,0 +1,16 @@
+//Creating a constant and capturing all the elements with the name .memory-card
+const cards = document.querySelectorAll('.memory-card');
+
+//Function to toggle the classlist with flip and removing it of item being clicked
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+/*Looping through each item in the cards array
+by listening to the click event and calling flipCard function */
+cards.forEach(card=> card.addEventListener('click', flipCard));
+
+
+
+
+
