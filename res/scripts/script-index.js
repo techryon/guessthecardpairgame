ca1e65f5a26a8d359cabc6e0202459b471a45c58
techryon <techryon@gmail.com>
2019-09-07 21:52:44 +1200

    Refactoring

--- res/scripts/script-index.js
+++ res/scripts/script-index.js
@@ -24,24 +24,38 @@ function flipCard() {
       hasFlippedCard = false; //setting it to false on 2nd click
       secondCard = this; //re-associating 'this' keyword to secondCard
   
-      //do cards match
-      if(firstCard.dataset.framework === 
-        secondCard.dataset.framework) {
-        //if the cards match
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-      
-      } else {
-          //not a match
-          setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-          }, 1500);
-          
-      }
+      //function to call to check for cards match
+      checkForMatch();
 
     }
 
+}
+
+//function which checks if the turned cards are a match
+function checkForMatch() {
+  if (firstCard.dataset.framework ===
+    secondCard.dataset.framework) {
+    //function to call when the cards match
+    disableCards();
+  } else {
+      //function to call when cards dont match
+      unflipCards();
+    }
+}
+
+
+//function which disables the click event on the cards, when they are matched
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  //setting timeout so we can see the 2nd card as well before the classes were reset
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 /*Looping through each item in the cards array
